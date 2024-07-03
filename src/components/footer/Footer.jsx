50c5cb7f29623655d321645f7f1ed372ac58bc12
Harshit Sharma <harshit2003s@gmail.com>
2024-07-03 20:23:55 +0530

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <a href="/" className='footer__logo'>Harshit Sharma</a>
 
         <ul className="permalinks">
-            <li><a href="#">Home</a></li>
+            <li><a href="/#">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#experience">Experience</a></li>
             <li><a href="#contact">Contact</a></li>
